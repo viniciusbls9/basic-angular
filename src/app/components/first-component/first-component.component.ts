f7ef7262eed11285b78695bb35343359355f6aee
Vinicius <beneditoloura@gmail.com>
2022-08-25 22:16:19 -0300

    feat: create first angular component

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css'],
+})
+export class FirstComponentComponent implements OnInit {
+  name: string = 'Vinicius';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
